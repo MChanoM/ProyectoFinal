eba4105092b5c0e584903e32c19660869ae9be8d
Mariano Mendoza <mariano.hw11@gmail.com>
2020-09-10 20:09:53 -0300

    se agregaron las ultimas configuraciones y se organizo la estructura de los archivos

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App () {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default App
