6b9b4bfe082423db56f73194e17f2fbdc79e5062
aguirre739 <pame11_@live.com>
2020-09-10 22:14:28 -0300

    se agrego carpeta abm y common

---
+++ src/components/abm/abmCategorias/AgregarCategoria.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AgregarNoticia = () => {
+    return (
+        <div>
+            <h1>esto es agregar categoria</h1>
+        </div>
+    );
+};
+
+export default AgregarNoticia;
---
+++ src/components/common/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h1>esto es el footer</h1>
+        </div>
+    );
+};
+
+export default Footer;
