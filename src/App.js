9bd5a33cc63092a79173ce5c53c5847781d24041
aguirre739 <pame11_@live.com>
2020-09-12 18:16:28 -0300

    se agregó navbar provisorio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap.min.css';
+import Header from './components/common/Header';
 
 function App () {
     return (
         <div>
-            
+            <Header></Header>
         </div>
     )
 }
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
+import Nav from "react-bootstrap/Nav";
+import Logo from "../../img/logo-sbg.png";
+// import Collapse from "react-bootstrap/Collapse";
 
 const Header = () => {
-    return (
-        <div>
-            <h1>esto es el navbar</h1>
-        </div>
-    );
+  //   const [open, setOpen] = useState(false);
+
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="#home">
+          <img
+            src={Logo}
+            width="80"
+            height="50"
+            className="d-inline-block align-top"
+            alt="logo news pro"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#link">Actualidad</Nav.Link>
+            <Nav.Link href="#link">Espectáculos</Nav.Link>
+            <Nav.Link href="#link">Tecnología</Nav.Link>
+            <Nav.Link href="#link">Deportes</Nav.Link>
+            <NavDropdown title="Otras Categorías" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Politica</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Economía
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Salud</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Fotografía</NavDropdown.Item>
+          </NavDropdown>
+          </Nav>
+          
+
+          <Button className="btn btn-success mx-2">Suscribite</Button>
+          <Button variant="outline-info">Ingresar</Button>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
