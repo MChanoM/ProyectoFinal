c83354291d396ff820211f43002851ee4356bbd2
Leonardo Almiron <almiron.leonardo@gmail.com>
2020-09-12 12:06:33 -0300

    se comienza a trabajar en la funcionalidad de Edicion de noticias. Se crea rama nueva

---
+++ src/components/abm/abmNoticias/EditarNoticia.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const EditarNoticia = () => {
+    return (
+        <div>
+            <h1>Pagina de edicion de noticia</h1>
+            
+        </div>
+    );
+};
+
+export default EditarNoticia;
+<h1>Pagina de edicion de noticia</h1>
