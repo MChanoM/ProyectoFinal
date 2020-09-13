3af76f626ac408f4a5aae3215ed74688f4c40dbd
aguirre739 <pame11_@live.com>
2020-09-13 12:19:19 -0300

    se agrego la ruta de agregar noticia al app.js y se agrego el formulario para agregar noticia

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PaginaInicio from './components/principal/PaginaInicio';
+import AgregarNoticia from './components/abm/abmNoticias/AgregarNoticia';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App () {
     return (
-        <div>
-            <Header></Header>
-            <PaginaInicio></PaginaInicio>
-            <Footer></Footer>
-            
-        </div>
-    )
+        <Router>
+      <Header></Header>
+      <Switch>
+        <Route exact path="/">
+          <PaginaInicio></PaginaInicio>
+        </Route>
+        <Route exact path="/noticia/nueva">
+          <AgregarNoticia
+          ></AgregarNoticia>
+        </Route>
+      </Switch>
+      <Footer></Footer>
+    </Router>
+    );
 }
 
 export default App
