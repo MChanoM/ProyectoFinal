20859e8b1bd4e5d0689d48538b0d11582f0d1e19
Leonardo Almiron <almiron.leonardo@gmail.com>
2020-09-17 19:52:33 -0300

    se empieza a trabajar en la logica de edicion de la noticia, se va a hacer pull con cambios nuevos

--- src/components/abm/abmNoticias/EditarNoticia.js
+++ src/components/abm/abmNoticias/EditarNoticia.js
@@ -3,84 +3,85 @@ import { Container, Alert, Form, Button } from "react-bootstrap";
 import Swal from "sweetalert2";
 
 const EditarNoticia = (props) => {
-  //     const [categoria, setCategoria] = useState("");
-  //   const [error, setError] = useState(false);
-  //   //aqui creo los ref
-  //   const nombreProductoRef = useRef("");
-  //   const precioProductoRef = useRef("");
+  const [categoria, setCategoria] = useState("");
+  const [error, setError] = useState(false);
+  //aqui creo los ref
+  const [noticiaDestacada, setNoticiaDestacada] = useState(false);
+  const tituloRef = useRef("");
+  const autorRef = useRef("")
+  const descripcionBreveRef = useRef("")
+  const cuerpoNoticiaRef = useRef("")
+  const imagenRef = useRef("")
+  const fechaRef = useRef("")
 
-  //   const seleccionarCategoria = (e) => {
-  //     setCategoria(e.target.value);
-  //   };
+    const seleccionarCategoria = (e) => {
+      setCategoria(e.target.value);
+    };
 
-  //   const handleSubmit = async (e) => {
-  //     e.preventDefault();
-  //     //validar datos
-  //     let _categoria = categoria === "" ? props.producto.categoria : categoria;
-  //     console.log(_categoria);
-  //     console.log(nombreProductoRef);
-  //     console.log(nombreProductoRef.current.value);
-  //     console.log(precioProductoRef.current.value);
-  //     if (
-  //       nombreProductoRef.current.value.trim() === "" ||
-  //       precioProductoRef.current.value.trim() === "" ||
-  //       _categoria === ""
-  //     ) {
-  //       setError(true);
-  //       return;
-  //     }
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      //validar datos
+      let _categoria = categoria === "" ? props.producto.categoria : categoria;
+      console.log(_categoria);
+      console.log(tituloRef);
+      console.log(tituloRef.current.value);
+      console.log(descripcionBreveRef.current.value);
+      if (
+        tituloRef.current.value.trim() === "" ||
+        descripcionBreveRef.current.value.trim() === "" ||
+        _categoria === ""
+      ) {
+        setError(true);
+        return;
+      }
 
-  //     setError(false);
+      setError(false);
 
-  //     //preparar el objeto con los nuevos datos
-  //     const productoEditado = {
-  //       nombreProducto:nombreProductoRef.current.value,
-  //       precioProducto:precioProductoRef.current.value,
-  //       categoria: _categoria
-  //     }
+      //preparar el objeto con los nuevos datos
+      const productoEditado = {
+        titulo:tituloRef.current.value,
+        descripcion:descripcionBreveRef.current.value,
+        categoria: _categoria
+      }
 
-  //     try {
-  //       const cabecera = {
-  //         method: "PUT",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //         body: JSON.stringify(productoEditado)
-  //       };
-  //       const resultado = await fetch(
-  //         `http://localhost:4000/cafeteria/${props.producto.id}`,
-  //         cabecera
-  //       );
-  //       console.log(resultado);
-  //       if(resultado.status === 200){
-  //         //se modificaron correctamente los datos
-  //         props.setRecargarProductos(true);
-  //         Swal.fire(
-  //           'Producto modificado!',
-  //           'El producto se modifico correctamente',
-  //           'success'
-  //         )
-  //         props.history.push("/productos");
-  //       }
-  //     } catch (bug) {
-  //       console.log(bug);
-  //     }
-  //   };
+      try {
+        const cabecera = {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(productoEditado)
+        };
+        const resultado = await fetch(
+          `http://localhost:4000/cafeteria/${props.producto.id}`,
+          cabecera
+        );
+        console.log(resultado);
+        if(resultado.status === 200){
+          //se modificaron correctamente los datos
+          props.setRecargarProductos(true);
+          Swal.fire(
+            'Producto modificado!',
+            'El producto se modifico correctamente',
+            'success'
+          )
+          props.history.push("/productos");
+        }
+      } catch (bug) {
+        console.log(bug);
+      }
+    };
 
   return (
     <Container>
       <h1 className="w-100 text-center my-4">Edición de Notica</h1>
       <hr></hr>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Alert variant={"danger"}>Todos los campos son obligatorios</Alert>
         <Form.Group className="d-flex">
-        <Form.Label>Noticia destacada</Form.Label>
+          <Form.Label>Noticia destacada</Form.Label>
           <p className="mr-1 ml-4">No</p>
-          <Form.Check
-            type="switch"
-            id="destaca"
-            label="Si"
-          />
+          <Form.Check type="switch" id="destaca" label="Si" />
         </Form.Group>
 
         <Form.Group>
