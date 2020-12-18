a6d9a448e7e89bdfea005f1ee8e177b6a60d755e
aguirre739 <pame11_@live.com>
2020-12-18 10:00:29 -0300

    se agregaron mas años al select

--- src/components/principal/SuscribirsePlan1.js
+++ src/components/principal/SuscribirsePlan1.js
@@ -106,7 +106,7 @@ const SuscribirsePlan1 = (props) => {
             onChange={(e) => {
               setSexo(e.target.value);
             }}>
-              <option>--seleccionar sexo--</option>
+              <option>--Seleccionar sexo--</option>
               <option>Hombre</option>
               <option>Mujer</option>
             </Form.Control>
@@ -176,7 +176,67 @@ const SuscribirsePlan1 = (props) => {
               onChange={(e) => {
                 setAnio(e.target.value);
               }}>
-                <option>--Anio--</option>
+                <option>--Año--</option>
+                <option>1920</option>
+                <option>1921</option>
+                <option>1922</option>
+                <option>1923</option>
+                <option>1924</option>
+                <option>1925</option>
+                <option>1926</option>
+                <option>1927</option>
+                <option>1928</option>
+                <option>1929</option>
+                <option>1930</option>
+                <option>1931</option>
+                <option>1932</option>
+                <option>1933</option>
+                <option>1934</option>
+                <option>1935</option>
+                <option>1936</option>
+                <option>1937</option>
+                <option>1938</option>
+                <option>1939</option>
+                <option>1940</option>
+                <option>1941</option>
+                <option>1942</option>
+                <option>1943</option>
+                <option>1944</option>
+                <option>1945</option>
+                <option>1946</option>
+                <option>1947</option>
+                <option>1948</option>
+                <option>1949</option>
+                <option>1950</option>
+                <option>1951</option>
+                <option>1952</option>
+                <option>1953</option>
+                <option>1954</option>
+                <option>1955</option>
+                <option>1956</option>
+                <option>1957</option>
+                <option>1958</option>
+                <option>1959</option>
+                <option>1960</option>
+                <option>1961</option>
+                <option>1962</option>
+                <option>1963</option>
+                <option>1964</option>
+                <option>1965</option>
+                <option>1966</option>
+                <option>1967</option>
+                <option>1968</option>
+                <option>1969</option>
+                <option>1970</option>
+                <option>1971</option>
+                <option>1972</option>
+                <option>1973</option>
+                <option>1974</option>
+                <option>1975</option>
+                <option>1976</option>
+                <option>1977</option>
+                <option>1978</option>
+                <option>1979</option>
                 <option>1980</option>
                 <option>1981</option>
                 <option>1982</option>
@@ -197,6 +257,10 @@ const SuscribirsePlan1 = (props) => {
                 <option>1997</option>
                 <option>1998</option>
                 <option>1999</option>
+                <option>2000</option>
+                <option>2001</option>
+                <option>2002</option>
+                <option>2003</option>
               </Form.Control>
             </div>
           </Form.Group>
