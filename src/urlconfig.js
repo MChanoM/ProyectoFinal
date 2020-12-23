d109bc792c67a27766e684b3fa97ac2a7367a4db
aguirre739 <pame11_@live.com>
2020-12-23 19:31:12 -0300

    se corrigio error al agregar noticia

--- src/urlconfig.js
+++ src/urlconfig.js
@@ -3,8 +3,8 @@
 const configs = {};
 
 
-// configs.urlBackend = 'https://newsprorc.herokuapp.com';
-configs.urlBackend = 'http://localhost:4000';
+configs.urlBackend = 'https://newsprorc.herokuapp.com';
+// configs.urlBackend = 'http://localhost:4000';
 
 
 export default configs;
